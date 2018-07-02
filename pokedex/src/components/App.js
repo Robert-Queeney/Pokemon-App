9bbfa0c2a2eb55ec6d6e8f40e4c049c62bd8480d
scoslo5512 <robert.queeney@gmail.com>
2018-07-01 17:52:38 -0700

    Started basic file structure

---
+++ pokedex/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hellohello
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
